d3847983c0b0d4ff7531bf33626990c71f1819c0
Jumbo125 <61667987+Jumbo125@users.noreply.github.com>
2025-04-09 12:45:37 +0200

    update main, with version 2

--- media/honeypot-token.php
+++ media/honeypot-token.php
@@ -3,8 +3,18 @@
 // Dieses Script wird von außen per JS aufgerufen ? liefert JSON mit Feldname + Token
 
 // Joomla initialisieren
+header('Content-Type: application/json');
 
-$configJson = file_get_contents("secret.txt");
+// Konfig aus Datei lesen (absoluter Pfad)
+$configFile = __DIR__ . '/secret.txt';
+
+if (!file_exists($configFile)) {
+    echo json_encode(['error' => 'Secret-Datei fehlt']);
+    exit;
+}
+
+
+$configJson = file_get_contents($configFile);
 
 $config = json_decode($configJson);
 
